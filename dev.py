7622425905612b7b7fe4bcaf7ed4475c86b5568e
AnTony <tavanan47@gmail.com>
2023-11-08 21:24:17 +0700

    lam database

--- dev.py
+++ dev.py
@@ -5,9 +5,10 @@ import matplotlib.pyplot as plt
 from flask_admin import Admin, AdminIndexView, expose, BaseView
 from datetime import datetime
 from flask_admin.contrib.sqla import ModelView
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'abcxzhjghehger'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:B20DCVT009@localhost/music'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:B20DCVT009@localhost/music2'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -16,7 +17,7 @@ admin = Admin(app, name='Quản trị người dùng', template_mode='bootstrap3
 
 # Class object cho bảng Users
 class User(db.Model):
-    __tablename__ = 'Users'
+    
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255), nullable=False)
     password = db.Column(db.String(255), nullable=False)
@@ -39,17 +40,15 @@ class Song(db.Model):
     __tablename__ = 'Song'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255), nullable=False)
-    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'))
-    artist = db.relationship('Artist',backref='songs')
+    image = db.Column(db.String(255), nullable=False)
+    link = db.Column(db.String(255), nullable=False)
     genre_id = db.Column(db.Integer, db.ForeignKey('genre.id'))
     genre = db.relationship('Genre')
     region_id = db.Column(db.Integer, db.ForeignKey('region.id'))
     region = db.relationship('Region')
-    link = db.Column(db.String(255), nullable=False)
     def __str__(self):
         return self.title
-
-# Class object cho bảng Genre
+    
 class Genre(db.Model):
     __tablename__ = 'genre'
     id = db.Column(db.Integer, primary_key=True)
@@ -66,29 +65,53 @@ class Region(db.Model):
         return self.name
 
 # Class object cho bảng DSYT (Danh sách yêu thích)
-class DSYT(db.Model):
-    __tablename__ = 'DSYT'
-    user_id = db.Column(db.Integer, db.ForeignKey('Users.id'), primary_key=True)
-    user = db.relationship('User')
+class Favorites(db.Model):
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id') , primary_key=True)
     song_id = db.Column(db.Integer, db.ForeignKey('Song.id'), primary_key=True)
+    user = db.relationship('User')
+    song = db.relationship('Song')
+
+class Performence(db.Model):
+    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id') , primary_key=True)
+    song_id = db.Column(db.Integer, db.ForeignKey('Song.id'), primary_key=True)
+    # Thêm quan hệ many-to-one với bảng Artist
+    artist = db.relationship('Artist')
+    # Thêm quan hệ many-to-one với bảng Song
     song = db.relationship('Song')
 
 class SongView(ModelView):
     edit_modal = True
     details_modal = True
     create_modal = True
+    # column_list = ['title', 'artists']
 
 class ArtistView(ModelView):
     edit_modal = True
-    details_modal = True
     create_modal = True
+    details_modal = True
 
-admin.add_view(ModelView(User, db.session))
+class DSYT_view(ModelView):
+    # form_columns = ['user_id','song_id']
+    # column_list = ['user_id','song_id']
+    pass
+
+class PerformanceModelView(ModelView):
+    # Đặt tên cho mục quản lý trên trang admin
+    column_label = "Performence"
+
+
+
+
+
 admin.add_view(SongView(Song, db.session))
 admin.add_view(ArtistView(Artist, db.session))
-admin.add_view(ModelView(Region, db.session))
-admin.add_view(ModelView(Genre , db.session))
-admin.add_view(ModelView(DSYT, db.session))
+admin.add_view(ModelView(Genre,db.session))
+admin.add_view(ModelView(Region,db.session))
+admin.add_view(DSYT_view(Favorites, db.session))
+admin.add_view(PerformanceModelView(Performence, db.session))
+
+
+
 
 
 
@@ -100,7 +123,6 @@ def get_songs():
     songs_list = []
     for song in songs:
         song_info = {
-            'id':song.id,
             'title': song.title,
             'artist': song.artist.name,  # Truy vấn tên nghệ sĩ từ quan hệ
             'genre': song.genre.name,    # Truy vấn tên thể loại từ quan hệ
@@ -128,33 +150,33 @@ def get_song(song_id):
     return jsonify(song_info)
 
 # xem các thể laoij nhạc
-@app.route('/genre/<int:genre_id>', methods=['GET'])
-def get_genre(genre_id):
-    genre = Genre.query.get(genre_id)
-    if genre is None:
-        return jsonify({'error' : 'Thể loại ko tồn tại'}),404
-    genre_info = {
-        'name' : genre.name,
-    }
-    return jsonify(genre_info)
+# @app.route('/genre/<int:genre_id>', methods=['GET'])
+# def get_genre(genre_id):
+#     genre = Genre.query.get(genre_id)
+#     if genre is None:
+#         return jsonify({'error' : 'Thể loại ko tồn tại'}),404
+#     genre_info = {
+#         'name' : genre.name,
+#     }
+#     return jsonify(genre_info)
 
 
-@app.route('/songs-by-genre', methods=['GET'])
-def songs_by_genre():
-    genre_name = request.args.get('genre')  # Lấy thể loại từ query parameter
-    songs = Song.query.join(Genre).filter(Genre.name == genre_name).all()
-    songs_list = []
-    for song in songs:
-        song_info = {
-            'title': song.title,
-            'artist': song.artist.name,
-            'genre': song.genre.name,
-            'region': song.region.name,
-            'link': song.link
-        }
-        songs_list.append(song_info)
+# @app.route('/songs-by-genre', methods=['GET'])
+# def songs_by_genre():
+#     genre_name = request.args.get('genre')  
+#     songs = Song.query.join(Genre).filter(Genre.name == genre_name).all()
+#     songs_list = []
+#     for song in songs:
+#         song_info = {
+#             'title': song.title,
+#             'artist': song.artist.name,
+#             'genre': song.genre.name,
+#             'region': song.region.name,
+#             'link': song.link
+#         }
+#         songs_list.append(song_info)
 
-    return jsonify({'songs': songs_list})
+#     return jsonify({'songs': songs_list})
 
 
 
